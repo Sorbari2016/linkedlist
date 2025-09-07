51aeb03ebea6241484cdf26f77ab5207d53b1c48
Clematins <152311585+Sorbari2016@users.noreply.github.com>
2025-09-07 16:06:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// Creating a linkedlist blueprint
+
+// Create a LinkedList class 
+class LinkedList {
+  constructor() {
+    this.head = null; 
+    this.size = 0; 
+  }
+
+  // append method
+  append(value) {
+    
+}
+
+// Create a Node class 
+class Node {
+  constructor(value) {
+    this.value = null; 
+    this.nextNode = null; 
+  }
+}
+
+
+
 
